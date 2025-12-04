a345e439c478d83bc197c7467a0b9f83053808d7
dilarademrts <dilarademrtas@gmail.com>
2025-12-04 23:57:36 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors";
+import productRoutes from "./modules/product/product.router.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/products", productRoutes);
+
+// Health check
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
