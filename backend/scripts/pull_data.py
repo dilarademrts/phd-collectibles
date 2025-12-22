57bbc20e767940697227a8c78dd09b80a10c4721
dilarademrts <dilarademrtas@gmail.com>
2025-12-23 01:57:31 +0300

    data

--- backend/scripts/pull_data.py
+++ backend/scripts/pull_data.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 # --- AYARLAR ---
 hedef_sitemap = "https://www.phdcollectibles.com/xml/sitemap_product_1.xml"
-LIMIT = 100  # Kaç ürün çekilsin? (İsteğine göre artırabilirsin)
+LIMIT = 1000  # Kaç ürün çekilsin? (İsteğine göre artırabilirsin)
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
